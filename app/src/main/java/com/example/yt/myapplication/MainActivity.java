f4b0773c73d76a86f0d8ecb34ea3450407393b99
yt <1398981815@qq.com>
2019-01-02 10:05:38 +0800

    增加一个界面

--- app/src/main/java/com/example/yt/myapplication/MainActivity.java
+++ app/src/main/java/com/example/yt/myapplication/MainActivity.java
@@ -1,13 +1,24 @@
 package com.example.yt.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button btn_one;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn_one=findViewById(R.id.btn_one);
+        btn_one.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getApplicationContext(),Main2Activity.class);
+            }
+        });
     }
 }
